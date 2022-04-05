9c6b44f303634c0b54c89e4b4a9d4cf46810cce9
Ritik Khadgi <kritik18@tbc.edu.np>
2022-04-05 20:18:25 +0545

    Two Activities Challenge Code

---
+++ app/src/main/java/com/example/twoactivitiescodingchallenge/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.twoactivitiescodingchallenge;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Parcelable;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private final static String LOG_TAG=MainActivity.class.getSimpleName();
+    private final static String EXTRA_MESSAGE ="com.example.android.twoactivitiescodingchallenge.extra.MESSAGE";
+    private  View paragraph;
+    private View article_heading;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void LaunchParaTwo(View view) {
+        Log.d(LOG_TAG,"Paragraph Two");
+        Intent intent = new Intent(this, SecondActivity.class);
+        paragraph =findViewById(R.id.article_text);
+        intent.putExtra(EXTRA_MESSAGE, (Parcelable) paragraph);
+        startActivity(intent);
+    }
+
+    public void LaunchParaOne(View view){
+        Log.d(LOG_TAG, "Paragraph One");
+        Intent intent = new Intent(this, SecondActivity.class);
+        paragraph =findViewById(R.id.article_text);
+        intent.putExtra(EXTRA_MESSAGE, (Parcelable) paragraph);
+        startActivity(intent);
+    }
+
+    public void LaunchParaThree(View view){
+        Log.d(LOG_TAG, "Paragraph Three");
+        Intent intent = new Intent(this, SecondActivity.class);
+        paragraph =findViewById(R.id.article_text);
+        intent.putExtra(EXTRA_MESSAGE,(Parcelable) paragraph);
+        startActivity(intent);
+    }
+}
